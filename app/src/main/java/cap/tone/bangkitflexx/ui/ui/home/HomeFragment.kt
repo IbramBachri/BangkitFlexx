98db81dde71be8a59d7a10f89f71fede22406029
IbramBachri <ibrambachri@gmail.com>
2022-06-07 20:42:01 +0700

    Drawer

--- app/src/main/java/cap/tone/bangkitflexx/ui/ui/home/HomeFragment.kt
+++ app/src/main/java/cap/tone/bangkitflexx/ui/ui/home/HomeFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import cap.tone.bangkitflexx.ListUserAdapter
 import cap.tone.bangkitflexx.R
