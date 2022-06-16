cda1cc6cdd4c3656e05f2df5b575ac92cf82c311
IbramBachri <ibrambachri@gmail.com>
2022-06-16 21:04:29 +0700

    Project Manager 2

---
+++ app/src/main/java/cap/tone/bangkitflexx/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package cap.tone.bangkitflexx.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cap.tone.bangkitflexx.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
